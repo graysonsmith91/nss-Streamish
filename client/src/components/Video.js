c3918baaa9427fef9a8a2e067e472d5bfa7e399b
Grayson Smith <graysonsmith91@gmail.com>
2023-04-12 16:03:36 -0500

    created react app and got videos to display

---
+++ client/src/components/Video.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+const Video = ({ video }) => {
+    return (
+        <Card >
+            <p className="text-left px-2">Posted by: {video.userProfile.name}</p>
+            <CardBody>
+                <iframe className="video"
+                    src={video.url}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen />
+
+                <p>
+                    <strong>{video.title}</strong>
+                </p>
+                <p>{video.description}</p>
+                <ul>
+                    <li>{video.comments}</li>
+                </ul>
+            </CardBody>
+        </Card>
+    );
+};
+
+// const displayComments = ({ video }) => {
+//     for (const comment of video.comments) {
+//         return <li>{comment}</li>
+//     }
+// }
+
+export default Video;
